289e9b031fc0e64c2c0a2939d61b4a0543489cc4
Alex Lu <alu59@my.bcit.ca>
2025-12-04 02:41:52 -0800

    Update docstring startup.py

--- game/scene/scenes/startup.py
+++ game/scene/scenes/startup.py
@@ -40,6 +40,10 @@ def get_startup_scene():
 
         None is returned to signify program exit.
 
+        :param game_data: a dictionary representing the data needed to run the game
+        :precondition game_data: must be a well-formed dictionary of game data
+        :postcondition: run the startup scene
+        :postcondition: return the next scene to run, or None for game exit
         :return: a string representing the name of the next scene to run,
                  or None to signify game exit
         """
