6f572a0641573b72973520a4b9253f049f4c53c4
Alex Lu <alu59@my.bcit.ca>
2025-12-04 00:27:35 -0800

    Moved init scenes to scene.py

--- game/scene/__init__.py
+++ game/scene/__init__.py
@@ -1,18 +1,3 @@
 """
-Game scenes (systems).
+Game scenes (systems) manager.
 """
-from game.scene import startup, main_menu
-
-
-def init_scenes():
-    """
-    Return a dictionary of scene data dictionaries for the game.
-
-    :postcondition: get a dictionary of well-formed scene data dictionaries
-    :return: a dictionary of scene data dictionaries representing the game's scenes
-    """
-    return {
-        "startup": startup.get_startup_scene(),
-        "main_menu": main_menu.get_main_menu_scene(),
-    }
-
