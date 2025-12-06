c00504f9c3b19fdaafbecd19a3f64e3ab0a8906d
Alex Lu <alu59@my.bcit.ca>
2025-12-05 22:33:12 -0800

    Change startup to use regular input

--- game/scene/scenes/startup.py
+++ game/scene/scenes/startup.py
@@ -1,8 +1,6 @@
 """
 Startup boot sequence and cool stuff.
 """
-from game.terminal.draw import create_text_area, draw_text_box
-from game.terminal.input import pull_input, start_text_input, poll_key_press
 from game.terminal.screen import get_screen_size, clear_screen
 
 
@@ -21,42 +19,24 @@ def get_startup_scene():
     :postcondition: get data for the startup scene
     :return: a dictionary representing the data for the startup scene
     """
-    prompt_text_area = create_text_area(column=1, row=1, width=get_screen_size()[0], height=2, text="")
-    update_confirm_prompt = start_text_input(3, 3)
 
-    def open_startup(_):
-        """
-        Start text input for screen size confirmation.
-
-        :postcondition: start text_input for screen size confirmation
-        """
-        nonlocal update_confirm_prompt
-        clear_screen()
-        update_confirm_prompt = start_text_input(3, 3)
-        update_confirm_prompt("escape")
-
-    def update_startup(game_data):
+    def update_startup(_):
         """
         Return the next scene to run after the startup.
 
         None is returned to signify program exit.
 
-        :param game_data: a dictionary representing the data needed to run the game
-        :precondition game_data: must be a well-formed dictionary of game data
         :postcondition: run the startup scene
         :postcondition: return the next scene to run, or None for game exit
         :return: a string representing the name of the next scene to run,
                  or None to signify game exit
         """
         while True:
-            prompt_text_area["text"] = (
+            clear_screen()
+            print(
                 f"Current size: {get_screen_size()} | Adjust to at least (100, 35) for the best experience.\n"
-                "Enter 'y' to continue, 'n' to exit, and any key to reload screen size.\n"
-                "> ")
-            draw_text_box(text_area=prompt_text_area, overwrite=True)
-            poll_key_press(game_data["key_input"])
-            inputted = pull_input(game_data["key_input"], flush=True)[0]
-            choice = update_confirm_prompt(inputted)
+                "Enter 'y' to continue, 'n' to exit, and any key to reload screen size.\n")
+            choice = input("> ")
             if choice is None:
                 continue
             choice = choice.lower()
@@ -64,12 +44,10 @@ def get_startup_scene():
                 return "main_menu"
             if choice == "n":
                 return "quit"
-            else:
-                open_startup(game_data)
 
     return {
         "name": "startup",
-        "open": open_startup,
+        "open": None,
         "update": update_startup,
         "exit": None}
 
