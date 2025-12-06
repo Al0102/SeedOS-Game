f735d4b80c45c975fd826739705e56a383e12d73
Alex Lu <alu59@my.bcit.ca>
2025-12-06 02:29:38 -0800

    Start creating burrow board

---
+++ game/seedOS/burrow/__init__.py
@@ -0,0 +1,3 @@
+"""
+Burrow editor (battles).
+"""
---
+++ game/seedOS/burrow/board.py
@@ -0,0 +1,192 @@
+"""
+Burrow board functions.
+"""
+from io import StringIO
+from sys import stderr
+from typing import TextIO
+
+from game.ansi_actions.cursor import cursor_set
+from game.ansi_actions.style import style
+from game.terminal.screen import point_within_screen, clear_screen
+from game.utilities import sum_vectors, remove_escape_codes
+
+
+def get_entity_types():
+    """
+    Get a dictionary of new entities.
+
+    Defined entities:
+        "floor",
+        "void",
+        "wall",
+        "small_bug",
+        "large_bug",
+        "nectar",
+        "player"
+
+    :postcondition: a dictionary representing the defined entities
+    :return: a dictionary representing the defined entities
+    """
+    entities = (
+        create_entity("floor", style("▯", "dim")),
+        create_entity("void", ""),
+        create_entity("wall", "#", health=5),
+        create_entity("small_bug", style("*", "red"), health=1, base_damage=1),
+        create_entity("large_bug", style("@", "red"), health=5, base_damage=3),
+        create_entity("nectar", style("n", "yellow"), heal=2),
+        create_entity("player", style("n", "green"), health=100))
+    return {entity["name"]: entity for entity in entities}
+
+
+def create_entity(name: str, icon: str, **kwargs) -> dict:
+    """
+    Create an entity.
+
+    Every entity has a name and icon.
+
+    :param name: a string representing the name of the entity
+    :param icon: a string representing the icon to display on the board for the entity
+    :param kwargs: a dictionary of entity properties
+    :precondition: name must be a string
+    :precondition: icon must be a string
+    :precondition: position must be a tuple of 2 integers larger than or equal to 1
+    :precondition: kwargs must be a dictionary
+    :postcondition: create an entity dictionary
+    :return: a dictionary representing an entity on a board
+
+    >>> create_entity("wall", "#") == {
+    ... "name":  "wall",
+    ... "icon": "#"}
+    True
+    >>> create_entity("small bug", "*", position=(10, 13), health=1, base_damage=1) == {
+    ... "name":  "small bug",
+    ... "icon": "*",
+    ... "position": (10, 13),
+    ... "health": 1,
+    ... "base_damage": 1}
+    True
+    """
+    return {"name": name, "icon": icon, **kwargs}
+
+
+def spawn_entity(board: dict, position: tuple, name=None, entity=None) -> dict | None:
+    """
+    Spawn an entity on the board.
+
+    :param board: a dictionary with position keys representing the board.
+    :param position: a tuple of two positive integers larger than 0 representing the spawn position of the entity
+    :param name: (default None) a string representing the name of the entity to spawn
+    :param entity: (default None) a dictionary representing entity to spawn
+    :precondition: board must be a dictionary of form <position tuple>: <entity dictionary>
+    :precondition: position must be a tuple of 2 positive integers larger than 0
+    :precondition: name must be a string or None
+    :precondition: name must be a defined entity in get_entity_types()
+    :precondition: entity must be a dictionary of entity data with at least these key-value-pairs:
+                   "name": <string> and "icon": <string>
+    :precondition: either <name> or <entity> must have a value
+    :postcondition: create a new entity of type <name> or spawnat <position> in the board
+    :return: a dictionary representing the created enemy if <name> is passed,
+             a dictionary representing the passed entity if <entity> is passed,
+             or None if the <name> is not a defined entity and <entity> is not passed
+    """
+    if not name is None:
+        try:
+            entity = get_entity_types()[name]
+        except KeyError:
+            print(f"Entity {name} not defined", file=stderr)
+    try:
+        entity["position"] = position
+    except KeyError:
+        print(f"Expected <name> or <entity> to be passed, but neither were found", file=stderr)
+    else:
+        board[position] = entity
+    return entity
+
+
+def load_board_from_file(board_file: TextIO):
+    """
+    Create a board with spawned entities based on a text file.
+
+    :param board_file: a TextIO object representing the tilemap to create a board from
+    :precondition: board_file must be a TextIO object
+    :precondition: get_entity_types must be defined
+    :postconditon: create a board from <board_file>
+    :postconditon: whitespace ("void" entities) will be ignored
+    :postconditon: if character is not the icon for an existing entity,
+                   create an entity with a name and icon set to that character
+    :return: a dictionary representing the board with entities loaded from <board_file>
+
+    >>> tilemap = StringIO("    ")
+    >>> load_board_from_file(tilemap) == {}
+    True
+    >>> tilemap = StringIO("#")
+    >>> load_board_from_file(tilemap) == {
+    ...     (1, 1): {'name': 'wall', 'icon': '#', 'health': 5, 'position': (1, 1)}}
+    True
+    >>> tilemap = StringIO(" * \\n   #")
+    >>> load_board_from_file(tilemap) == {
+    ...     (2, 1): {'name': 'small_bug', 'icon': '\x1b[31m*\x1b[0m',
+    ...              'health': 1, 'base_damage': 1, 'position': (2, 1)},
+    ...     (4, 2): {'name': 'wall', 'icon': '#', 'health': 5, 'position': (4, 2)}}
+    True
+    """
+    board = {}
+    entity_icons = dict(map(
+        lambda entity: (remove_escape_codes(entity["icon"]), entity["name"]),
+        get_entity_types().values()))
+    row = 1
+    while line := board_file.readline():
+        first_character_index, last_character_index = len(line) - len(line.lstrip()), len(line.rstrip())
+        for column, character in enumerate(line[first_character_index:last_character_index], first_character_index):
+            try:
+                spawn_entity(board, name=entity_icons[character], position=(column + 1, row))
+            except KeyError:
+                spawn_entity(
+                    board,
+                    entity=create_entity(name=character, icon=character),
+                    position=(column + 1, row))
+        row += 1
+    return board
+
+
+
+def draw_board(board: dict, position_offset=(0, 0), flush=True):
+    """
+    Draw the values of board to the screen, offset by <position_offset>.
+
+    :param board: a dictionary of entity dictionaries representing the board to draw
+    :param position_offset: a tuple of 2 integers larger than or equal to 0,
+           representing the position to offset drawing <board> by
+    :param flush: (default True) a boolean representing whether to flush the output to stdout
+    :precondition: board must be a dictionary of dictionaries with key-value pair: "icon": <string>
+    :precondition: position_offset must be a tuple of 2 integers larger than or equal to 0,
+           representing the position to offset drawing <board> by
+    :precondition: flush_output must be a boolean
+    :postcondition: draw <board> to the screen, offset by <position_offset>
+    """
+    for entity_position, entity in board.items():
+        terminal_position = sum_vectors(entity_position, position_offset)
+        if all(point_within_screen(terminal_position)):
+            cursor_set(*terminal_position)
+            print(entity["icon"], end="")
+    print(end="", flush=flush)
+
+
+def main():
+    """
+    Drive the program.
+    """
+    clear_screen()
+    draw_board({(1, 1): {"icon": "a"}})
+    input()
+    clear_screen()
+    draw_board({(4, 2): {"icon": "a"}})
+    input()
+    clear_screen()
+    board = {}
+    spawn_entity(board, name="wall", position=(1, 1))
+    spawn_entity(board, name="wall", position=(1, 2))
+    draw_board(board)
+
+if __name__ == '__main__':
+    main()
