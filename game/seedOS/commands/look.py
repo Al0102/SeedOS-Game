b9d937b23c31de8f18881f40ed8b4466a8ad54cc
Alex Lu <alu59@my.bcit.ca>
2025-12-06 17:16:24 -0800

    Fix whitespace login

---
+++ game/seedOS/commands/look.py
@@ -0,0 +1,60 @@
+"""
+Look at the content of a text file.
+"""
+from game.ansi_actions.style import style
+from game.seedOS import create_command
+from game.seedOS.files import convert_relative_path_to_absolute
+
+
+def get_look_command():
+    """
+    Get the dictionary data for the look command.
+
+    :postcondition: get the data for the look command
+    :return: a dictionary representing the data for the look command
+    """
+    return create_command(
+        name="look",
+        run=run_look,
+        privilege_required=0)
+
+
+def run_look(seed_system, tokens):
+    """
+    Run the look command.
+
+    Look through the content of a text file.
+
+    :param seed_system: a dictionary representing the currently active seedOS system
+    :param tokens: a list of strings representing the arguments passed in to the command
+    :precondition: seed_system must be a well-formed seed_system dictionary
+    :precondition: tokens must be a list of strings
+    :postcondition: look through the content of a text file
+    :return: a tuple of 2 strings representing the success status and a status message
+    """
+    status = "success"
+    if len(tokens) != 1:
+        status = "argument_error"
+        status_message = f"|'look' expects exactly 1 argument: [path]|\n{len(tokens)} != 1"
+    else:
+        new_path = convert_relative_path_to_absolute(seed_system["aphid"]["current_folder"], tokens[0])
+        try:
+            path_data = seed_system["file_tree"][new_path]
+        except KeyError:
+            status = "argument_error"
+            status_message = f"|Invalid path|\n{style(new_path, 'underline')}"
+        else:
+            if path_data["type"] != "file" or path_data["extension"] != "txt":
+                status = "argument_error"
+                status_message = f"|Can't read non-txt files|\n{style(new_path, 'underline')}"
+            elif path_data["privilege_required"] > seed_system["aphid"]["privilege"]:
+                status = "privilege_error"
+                status_message = (
+                    "|Privilege too low|\n"
+                    f"{seed_system['aphid']['privilege']} < {path_data['privilege_required']}")
+            else:
+                seed_system["aphid"]["current_folder"] = new_path
+                status_message = f"|Changed folder to path|\n{style(new_path, 'underline')}"
+    return (status, status_message)
+
+
